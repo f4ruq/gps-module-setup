d0307dfd912271c879294f5219bc9516f40cba16
Omer F. Yuce <68684512+f4ruq@users.noreply.github.com>
2025-08-11 01:24:02 +0300

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,5 +1,7 @@
 #include <TinyGPSPlus.h>
 #include <HardwareSerial.h>
+#define RXPIN 17
+#define TXPIN 18
 
 TinyGPSPlus gps;
 HardwareSerial GPSSerial(1);                  // UART1
@@ -7,21 +9,26 @@ HardwareSerial GPSSerial(1);                  // UART1
 void setup() {
   Serial.begin(115200);                       // USB-CDC (PC'ye log)
   GPSSerial.setRxBufferSize(1024);
-  GPSSerial.begin(9600, SERIAL_8N1, 17, 18);  // RX=17 (GPS TX), TX=18 (GPS RX - opsiyonel)
-  Serial.println("GPS verisi bekleniyor...");
+  GPSSerial.begin(9600, SERIAL_8N1, RXPIN, TXPIN);  // RX=17 (GPS TX), TX=18 (GPS RX)
+  Serial.println("waiting for gps data..");
 }
 
 void loop() {
-  while (GPSSerial.available() > 0) {
+  while (GPSSerial.available()) {
     char c = GPSSerial.read();
     gps.encode(c);
 
-    if (gps.location.isUpdated()) {
-      Serial.print("Enlem: ");  Serial.println(gps.location.lat(), 6);
-      Serial.print("Boylam: "); Serial.println(gps.location.lng(), 6);
-      Serial.print("Uydu sayısı: "); Serial.println(gps.satellites.value());
-      Serial.print("HDOP: ");   Serial.println(gps.hdop.hdop());
+    if (gps.location.isUpdated()) 
+    {
+      Serial.print("coordinates: ");
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(" ,");
+      Serial.println(gps.location.lng(), 6);
+      Serial.print("number of satellites : ");
+      Serial.println(gps.satellites.value());
+      Serial.print("HDOP: ");
+      Serial.println(gps.hdop.hdop());
       Serial.println("----------------------");
     }
   }
-}
+} 
